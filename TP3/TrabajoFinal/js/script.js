b82266018e2489640274b407d30248714d72bcd8
Agustin Miguel <agustinmiguel96@gmail.com>
2020-10-12 10:47:49 -0300

    commit 2

--- TP3/TrabajoFinal/js/script.js
+++ TP3/TrabajoFinal/js/script.js
@@ -4,3 +4,16 @@ window.addEventListener("scroll", function(){
     }  
 });
 
+window.addEventListener("scroll", function(){
+    if (document.body.scrollTop > 75|| document.documentElement.scrollTop > 75){
+        document.querySelector("#flip3Dcontainer").className = "flip3Dcontainer";
+    }  
+});
+
+let bg =  document.querySelector("#bg");
+window.addEventListener("scroll", function(){
+    if (document.body.scrollTop > 75|| document.documentElement.scrollTop > 75){
+        bg.style.backgroundPosition = +window.pageYOffset+"px";
+    }  
+});
+

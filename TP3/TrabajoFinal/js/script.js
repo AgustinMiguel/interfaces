4363d7e108bbd3cc5a92f76a0c0140d8699d78a7
Agustin Miguel <agustinmiguel96@gmail.com>
2020-10-12 20:51:27 -0300

    added carrusel

--- TP3/TrabajoFinal/js/script.js
+++ TP3/TrabajoFinal/js/script.js
@@ -10,10 +10,42 @@ window.addEventListener("scroll", function(){
     }  
 });
 
-let bg =  document.querySelector("#bg");
+const bg =  document.querySelector("#bg");
 window.addEventListener("scroll", function(){
-    if (document.body.scrollTop > 75|| document.documentElement.scrollTop > 75){
+    if (document.body.scrollTop > 100|| document.documentElement.scrollTop > 100){
         bg.style.backgroundPosition = +window.pageYOffset+"px";
     }  
 });
+
+const carruselSlide = document.querySelector(".carrusel-slide");
+const carruselImg = document.querySelectorAll(".carrusel-slide img");
+const prevBtn = document.querySelector("#prevBtn");
+const nextBtn = document.querySelector("#nextBtn");
+
+let counter = 1;
+
+const size = carruselImg[0].clientWidth;
+
+carruselSlide.style.transform = "translateX(" +(-size * counter) + "px"; //PARA COMENZAR EN LA PRIMER IMG DEL CARRUSEL
+
+nextBtn.addEventListener("click", function(){
+    if(counter === carruselImg.length){
+        counter = 1;
+    }
+    carruselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter++;
+    carruselSlide.style.transform = "translateX(" +(-size * counter) + "px";
+});
+
+prevBtn.addEventListener("click", function(){
+    if(counter === 1){
+        counter = carruselImg.length -2;
+    }
+    carruselSlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+    carruselSlide.style.transform = "translateX(" +(-size * counter) + "px";
+});
+
+
+
 

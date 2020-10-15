3925f9dac130a9b29455add153b2f24ac9dc850a
Agustin Miguel <agustinmiguel96@gmail.com>
2020-10-15 18:23:31 -0300

    burguer rdy

--- TP3/TrabajoFinal/js/script.js
+++ TP3/TrabajoFinal/js/script.js
@@ -1,8 +1,4 @@
-window.addEventListener("scroll", function(){
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50){
-        document.querySelector("#buttons").className = "navbarcontainter";
-    }  
-});
+
 
 window.addEventListener("scroll", function(){
     if (document.body.scrollTop > 75|| document.documentElement.scrollTop > 75){
@@ -29,7 +25,8 @@ const size = carruselImg[0].clientWidth;
 carruselSlide.style.transform = "translateX(" +(-size * counter) + "px"; //PARA COMENZAR EN LA PRIMER IMG DEL CARRUSEL
 
 nextBtn.addEventListener("click", function(){
-    if(counter === carruselImg.length){
+    console.log(carruselImg.length);
+    if(counter === carruselImg.length -2){
         counter = 1;
     }
     carruselSlide.style.transition = "transform 0.4s ease-in-out";
@@ -46,6 +43,29 @@ prevBtn.addEventListener("click", function(){
     carruselSlide.style.transform = "translateX(" +(-size * counter) + "px";
 });
 
+const acordionItemsHeader = document.querySelectorAll(".acordion-item-header");
+acordionItemsHeader.forEach(acordionItemsHeader => {
+    acordionItemsHeader.addEventListener("click", event =>{
+        acordionItemsHeader.classList.toggle("active");
+    });
+});
+
+let countDownDate = new Date("dec 5, 2020 15:00:00").getTime();
+let x = setInterval(function() {
+    let now = new Date().getTime();
+    let distance = countDownDate - now;
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    hours += days * 24;
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    document.getElementById("countdown").innerHTML = hours + "h "
+    + minutes + "m " + seconds + "s ";
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+  }, 1000);
 
 
 

e8f17f725fbaf0d995cd19b3a0c08aaa027677fd
Agustin Miguel <agustinmiguel96@gmail.com>
2020-10-15 20:40:59 -0300

    y porque ahora siii

--- TP3/TrabajoFinal/js/script.js
+++ TP3/TrabajoFinal/js/script.js
@@ -3,7 +3,7 @@ const loader = document.querySelector(".loader");
 setInterval(function(){
     web.className = "web";
     loader.className = "loaderHidden"
-}, 3);
+}, 3000);
 
 window.addEventListener("scroll", function(){
     if (document.body.scrollTop > 75|| document.documentElement.scrollTop > 75){

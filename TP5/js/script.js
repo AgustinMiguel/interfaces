21456bbc5b8ec0627f4bde3648a1a49e455772eb
Agustin Miguel <agustinmiguel96@gmail.com>
2020-11-25 16:56:40 -0300

    index rdy

--- TP5/js/script.js
+++ TP5/js/script.js
@@ -1,7 +1,3 @@
-let slider1 = document.querySelector("#slider1");
-let innerSlider1 = document.querySelector("#slider-inner1");
-let slider2 = document.querySelector("#slider2");
-let innerSlider2 = document.querySelector("#slider-inner2");
 let slider3 = document.querySelector("#slider3");
 let innerSlider3 = document.querySelector("#slider-inner3");
 let slider4 = document.querySelector("#slider4");
@@ -16,6 +12,8 @@ let likeNavBar = false;
 let pressed = false;
 let startx;
 let x;
+
+
 
 like.addEventListener('mouseenter', (e)=>{
     if(likeNavBar===false)
@@ -55,53 +53,6 @@ play.addEventListener('click', (e)=>{
 
 myProgress.addEventListener('click', (e)=>{
    myBar.style.backgroundPosition = -400+e.offsetX+"px";
-});
-
-
-slider1.addEventListener('mousedown', (e)=>{
-    pressed = true;
-    startx = e.offsetX - innerSlider1.offsetLeft;
-    slider1.style.cursor = 'grabbing';
-});
-
-
-slider1.addEventListener('mouseup', ()=>{
-    slider1.style.cursor = 'initial';
-});
-
-slider1.addEventListener('mouseup', ()=>{
-    pressed= false;
-});
-
-slider1.addEventListener('mousemove', (e)=>{
-   if(!pressed) return;
-   e.preventDefault();
-   x= e.offsetX;
-    innerSlider1.style.left = `${x-startx}px`;
-    checklimit(slider1,innerSlider1);
-});
-
-slider2.addEventListener('mousedown', (e)=>{
-    pressed = true;
-    startx = e.offsetX - innerSlider2.offsetLeft;
-    slider2.style.cursor = 'grabbing';
-});
-
-
-slider2.addEventListener('mouseup', ()=>{
-    slider2.style.cursor = 'inital';
-});
-
-slider2.addEventListener('mouseup', ()=>{
-    pressed= false;
-});
-
-slider2.addEventListener('mousemove', (e)=>{
-   if(!pressed) return;
-   e.preventDefault();
-   x= e.offsetX;
-    innerSlider2.style.left = `${x-startx}px`;
-    checklimit(slider2,innerSlider2);
 });
 
 slider3.addEventListener('mousedown', (e)=>{
@@ -158,8 +109,6 @@ function checklimit(slider, innerSlider){
         innerSlider.style.left = `-${inner.width - outer.width}px`;
     }
 }
-checklimit(slider1,innerSlider1);
-checklimit(slider2,innerSlider2);
 checklimit(slider3,innerSlider3);
 checklimit(slider4,innerSlider4);
 

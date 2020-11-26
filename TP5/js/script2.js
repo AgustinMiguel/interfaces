ae05a15a08a047ae0499680341bf93bc951bf3ec
Matias Villegas <mati.villegas23@gmail.com>
2020-11-25 21:35:59 -0300

    playlist 90%

--- TP5/js/script2.js
+++ TP5/js/script2.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $(".trigger_popup_fricc").click(function() {
         $('.hover_bkgr_fricc').show();
     });
+    $(".trigger_popup_fricc2").click(function() {
+        $('.hover_bkgr_fricc').show();
+    });
     $('.hover_bkgr_fricc').click(function() {
         $('.hover_bkgr_fricc').hide();
     });
